8ecdb28956b4ead84afc16ea8d43c6fa96609724
william <jose.bonfim09@aluno.ifce.edu.br>
2026-09-15 11:13:38 -0300

    Adicionando a v1.1.0 da Agenda de Contatos

--- Principal.java
+++ Principal.java
@@ -1,43 +1,14 @@
 package br.edu.principal;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class Principal {
-	
-	public static void main(String[] args) {
+import javax.swing.JOptionPane;
 
-        List<String> nomes = new ArrayList<>();
-        List<String> celulares = new ArrayList<>();
-        List<String> emails = new ArrayList<>();
-        int opcao;
-        boolean continuar = true;
-        Scanner sc = new Scanner(System.in);
-
-        mostraInicializacao(); 
-
-        while (continuar) {
-        	mostraMenu();
-            opcao = selecionaOpcao(sc);
-
-            switch (opcao) {
-                case 1-> adicionar(sc, nomes, celulares, emails);           	
-                case 2-> listar(nomes, celulares, emails);
-                case 3-> pesquisar(sc, nomes, celulares, emails);
-                case 4-> atualizar(sc, nomes, celulares, emails);           	
-                case 5-> excluir(sc, nomes, celulares, emails);          	
-                case 6-> sair(continuar);
-                default -> System.out.println("Opção inválida!");
-            }
-        }
-        sc.close();
-    }
-    
-    public static void mostraInicializacao() {
+public class Uteis {
+	public static void mostraInicializacao() {
     	System.out.println("==========================");
         System.out.println("     AGENDA DE CONTATOS    ");
-        System.out.println("          v1.0.0           ");
+        System.out.println("          v1.1.0           ");
         System.out.println("==========================");
         System.out.println("Bem-vindo!");
     }
@@ -50,6 +21,7 @@ public class Principal {
         System.out.println("4 - Alterar contato");
         System.out.println("5 - Excluir contato");
         System.out.println("6 - Sair");
+        System.out.println("7 - Informações Sobre a Agenda de Contatos");
     }
     
     public static int selecionaOpcao(Scanner sc) {
@@ -58,134 +30,14 @@ public class Principal {
         sc.nextLine();
         return opc;
     }
-    
-    public static void adicionar(Scanner sc, List<String> nomes, List<String> celulares, List<String> emails) {
-    	System.out.println("=== ADICIONAR CONTATO ===");
-        System.out.print("Digite o nome: ");
-        String nome = sc.nextLine();
-        System.out.print("Digite o celular: ");
-        String celular = sc.nextLine();
-        System.out.print("Digite o email: ");
-        String email = sc.nextLine();
 
-        nomes.add(nome);
-        celulares.add(celular);
-        emails.add(email);
-
-        System.out.println("Contato adicionado com sucesso!");
-    }
-
-    public static void listar(
-            List<String> nomes,
-            List<String> celulares,
-            List<String> emails) {
-    	System.out.println("=== LISTAR CONTATOS ===");
-        if (nomes.size() == 0) {
-            System.out.println("Nenhum contato cadastrado!");
-
-        } else {
-            for (int i = 0; i < nomes.size(); i++) {
-
-                System.out.println("--------------------------");
-                System.out.println("Nome: " + nomes.get(i));
-                System.out.println("Celular: " + celulares.get(i));
-                System.out.println("Email: " + emails.get(i));
-            }
-        }
-    }
-
-    public static void pesquisar(
-            Scanner sc,
-            List<String> nomes,
-            List<String> celulares,
-            List<String> emails) {
-    	System.out.println("=== PROCURAR CONTATO ===");
-        System.out.print("Digite o nome do contato: ");
-        String nomeBusca = sc.nextLine();
-
-        boolean encontrado = false;
-
-        for (int i = 0; i < nomes.size(); i++) {
-            if (nomes.get(i).equalsIgnoreCase(nomeBusca)) {
-                System.out.println("--------------------------");
-                System.out.println("Nome: " + nomes.get(i));
-                System.out.println("Celular: " + celulares.get(i));
-                System.out.println("Email: " + emails.get(i));
-
-                encontrado = true;
-            }
-        }
-        if (!encontrado) {
-            System.out.println("Contato não encontrado!");
-        }
-    }
-
-    public static void atualizar(
-            Scanner sc,
-            List<String> nomes,
-            List<String> celulares,
-            List<String> emails) {
-    	System.out.println("=== ALTERAR CONTATO ===");
-        System.out.print("Digite o nome do contato: ");
-        String nomeProcurado = sc.nextLine();
-
-        int posicao = -1;
-        for (int i = 0; i < nomes.size(); i++) {
-            if (nomes.get(i).equalsIgnoreCase(nomeProcurado)) {
-                posicao = i;
-            }
-        }
-        if (posicao != -1) {
-
-            System.out.print("Digite o novo nome: ");
-            String novoNome = sc.nextLine();
-
-            System.out.print("Digite o novo celular: ");
-            String novoCelular = sc.nextLine();
-
-            System.out.print("Digite o novo email: ");
-            String novoEmail = sc.nextLine();
-
-            nomes.set(posicao, novoNome);
-            celulares.set(posicao, novoCelular);
-            emails.set(posicao, novoEmail);
-
-            System.out.println("Contato alterado com sucesso!");
-        } else {
-            System.out.println("Contato não encontrado!");
-        }
-    }
-
-    public static void excluir(
-            Scanner sc,
-            List<String> nomes,
-            List<String> celulares,
-            List<String> emails) {
-    	System.out.println("=== EXCLUIR CONTATO ===");
-        System.out.print("Digite o nome do contato: ");
-        String nomeExcluir = sc.nextLine();
-
-        boolean excluido = false;
-
-        for (int i = 0; i < nomes.size(); i++) {
-            if (nomes.get(i).equalsIgnoreCase(nomeExcluir)) {
-                nomes.remove(i);
-                celulares.remove(i);
-                emails.remove(i);
-
-                excluido = true;
-
-                System.out.println("Contato excluído com sucesso!");
-                break;
-            }
-        }
-        if (!excluido) {
-            System.out.println("Contato não encontrado!");
-        }
-    }
-    
     public static void sair(boolean continuar) {
     	System.out.println("Saindo da Agenda de Contatos...");
         continuar = false;
+    }
+    
+    public static void sobre() {
+    	JOptionPane.showMessageDialog(null, 
+    			"Desenvolvido por Roger M. Sarmento!");
     }
 }
